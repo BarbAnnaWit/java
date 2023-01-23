de25668112014582879b124816f953b5fc05bdb1
BarbaraWitek <barbara.witek9@gmail.com>
2023-01-23 14:01:37 +0100

    Git

--- src/YesNo.java
+++ src/YesNo.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 public class YesNo {
     public static void main(String[] args) {
-        Scanner
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Do you want to continue playing");
+        String answer = obj.nextLine();
+
+        if (!(answer.equals("Yes") || answer.equals("yes") || answer.equals("YES")))
+        {
+            System.out.println("The game will end here.");
+        }
+        else
+        {
+            System.out.println("Go to the next level!");
+        }
     }
 }
