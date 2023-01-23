dc84fe2b3d0aa72ad69814bd19758e6619fdf4f5
BarbaraWitek <barbara.witek9@gmail.com>
2023-01-23 14:01:53 +0100

    Git

---
+++ src/logicalNot.java
@@ -0,0 +1,13 @@
+
+
+public class logicalNot {
+    public static void main(String[] args) {
+        String text = "yes";
+        if (!text.equals("No") || text.equals("No")){
+            System.out.println(" Game continue");
+        }
+        else  {
+            System.out.println("End The Game");
+        }
+    }
+}
---
+++ src/logicalOr.java
@@ -0,0 +1,13 @@
+public class logicalOr {
+    public static void main(String[] args){
+        String shoesColour = "Black";
+        if ((shoesColour == "Black") || shoesColour == "Brown"){
+        System.out.println(" The colour is good");}
+            else
+        {
+                System.out.println("I don't like the colour");}
+
+        }
+
+    }
+
