4c04de6736c033c7c94a10c788b9829cb2658ee6
BarbaraWitek <barbara.witek9@gmail.com>
2023-02-14 15:54:17 +0100

    Git

--- src/elementsOfArray.java
+++ src/elementsOfArray.java
@@ -1,10 +1,12 @@
 public class elementsOfArray {
     public static void main(String[] args) {
 
-        String [] cars = {"Volvo", "BMW", "Ford", "Mazda","fiat"};
+        String [] cars = {"Volvo", "BMW", "Ford", "Mazda","Audi"};
 
         System.out.println(cars[0]);
-        System.out.println(cars[4]);
+        System.out.println(cars[2]);
+        System.out.println(cars[3]);
+
 
         System.out.println("chose the car" + cars.length );
     }
