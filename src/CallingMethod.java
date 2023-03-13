81115d69472b97f6209f26c97abee33ba15a3a10
BarbaraWitek <barbara.witek9@gmail.com>
2023-03-13 12:45:24 +0100

    Git

---
+++ src/CallingMethod.java
@@ -0,0 +1,15 @@
+public class CallingMethod {
+
+
+            static void myMethod() {
+                System.out.println("I just got executed!");
+            }
+            public static void main(String[] args) {
+                myMethod();
+                myMethod();
+                myMethod();
+            }
+        }
+
+
+
